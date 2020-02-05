da79fbc6a86438da6167b63f85e3894c0c920477
unknown <yonoraphael@gmail.com>
2020-02-06 03:35:42 +0700

    Updates

---
+++ precache-manifest.5cda8d4c2117ded04283efec3049d124.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d330cf0d7ba3469d303f2d42dc956b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c368a24012f90d1a0baf",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ad117a396bfc070b1c36",
+    "url": "/static/css/main.aeb615a2.chunk.css"
+  },
+  {
+    "revision": "c368a24012f90d1a0baf",
+    "url": "/static/js/2.f6eaa050.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/static/js/2.f6eaa050.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad117a396bfc070b1c36",
+    "url": "/static/js/main.64b8a915.chunk.js"
+  },
+  {
+    "revision": "8f62c4e616894d28f9a2",
+    "url": "/static/js/runtime-main.41189fb3.js"
+  },
+  {
+    "revision": "8adc7f17c8c945b91577e342d0752953",
+    "url": "/static/media/chef-1.8adc7f17.jpg"
+  },
+  {
+    "revision": "f8bb245b09e1c7270a505a9b82a54c8b",
+    "url": "/static/media/chef-2.f8bb245b.jpg"
+  },
+  {
+    "revision": "06f2fd4bddaf3f2db54cd0a7b583f79e",
+    "url": "/static/media/chef-3.06f2fd4b.jpg"
+  },
+  {
+    "revision": "38619bbcad2be602510d3dbcf2c7abc8",
+    "url": "/static/media/hero-1.38619bbc.jpg"
+  },
+  {
+    "revision": "627655c6b58da4fe2795079806360114",
+    "url": "/static/media/hero-2.627655c6.jpg"
+  },
+  {
+    "revision": "35a0a9dd344b8190abc707ac5dd91acb",
+    "url": "/static/media/hero-3.35a0a9dd.jpg"
+  },
+  {
+    "revision": "b3c83b70d704d23415811c67c4bc01e1",
+    "url": "/static/media/reservation-bg.b3c83b70.jpg"
+  },
+  {
+    "revision": "36fbcc187f07da933884615a8b3f92ff",
+    "url": "/static/media/salmon-zucchini.36fbcc18.jpg"
+  },
+  {
+    "revision": "0323abbe19967e2feff00221700fc738",
+    "url": "/static/media/steak.0323abbe.jpg"
+  },
+  {
+    "revision": "f9cc46efcbaec5f5b190f810f45e781a",
+    "url": "/static/media/testi-bg.f9cc46ef.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foodshop/precache-manifest.309a4fe9c7c85d5b5686877f09f85a15.js"
+  "/precache-manifest.5cda8d4c2117ded04283efec3049d124.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodshop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
